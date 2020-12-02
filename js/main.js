3644b3b40d5fc72ce50f2fd36b8a533a98fb8770
kabarter <50552547+kabarter@users.noreply.github.com>
2020-12-01 22:57:59 -0500

    after two sites and multiple attempts the code is finally pulling weather data

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 let city = document.getElementById('displayResults')
 
 function weather(city){
-    let key = '7ed6f6627d7c40973c307b393360c74e';
     fetch ('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=7ed6f6627d7c40973c307b393360c74e')
     .then(function(resp) {return resp.json()})
     .then(function(data){
-        console.log(data);
+        weatherData(data);
     })
-    .catch(function(){
+   // .catch(function(){
     
-    });
+    //});
 }
 window.onload = function(){
     weather(city);
@@ -19,6 +18,7 @@ function weatherData(data){
     let fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
     document.getElementById('description').innerHTML = data.weather[0].description;
     document.getElementById('returnedLocation').innerHTML = data.name;
-    document.getElementById('returnedTemp').innerHTML = fahrenheit + 'deg';
+    document.getElementById('returnedTemp').innerHTML = fahrenheit + ' deg';
+    document.getElementById('icon').innerHTML = description.icon;
 
 }
