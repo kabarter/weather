addad996ed6a1ff30297aff4a1347ba4ec462337
kabarter <50552547+kabarter@users.noreply.github.com>
2020-12-02 00:01:56 -0500

    working on search

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let city = document.getElementById('displayResults')
+//let city = document.getElementById('displayResults')
 
 function weather(city){
     fetch ('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=7ed6f6627d7c40973c307b393360c74e')
@@ -21,4 +21,24 @@ function weatherData(data){
     document.getElementById('returnedTemp').innerHTML = fahrenheit + ' deg';
     document.getElementById('icon').innerHTML = description.icon;
 
+}
+
+const search = document.querySelector('.searchCity');
+search.addEventListener('keypress', setQuery);
+
+function setQuery(event){
+    if (event.keyCode == 13){
+        getResults(search.value);
+    }
+}
+function getResults (results){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${api.7ed6f6627d7c40973c307b393360c74e}')
+    .then(results => {
+        return results.json();
+     }).then(displayResults);
+}
+
+function displayResults(weatherSearch) {
+    let city = document.querySelector('.location .city');
+    city.innerText = '${weatherSearch.name}, ${weatherSearch.sys.country}';
 }
