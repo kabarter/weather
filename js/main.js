acfe81d23611ecfaeb6cc58e8b6804ed9b9beaa4
kabarter <50552547+kabarter@users.noreply.github.com>
2020-12-01 21:55:40 -0500

    updated fetch

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function weather(city){
     let key = '7ed6f6627d7c40973c307b393360c74e';
-    fetch ('api.openweathermap.org/data/2.5/weather?q=Boston&appid=7ed6f6627d7c40973c307b393360c74e')
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=7ed6f6627d7c40973c307b393360c74e')
     .then(function(resp) {return resp.json()})
     .then(function(data){
         console.log(data);
