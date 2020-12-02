35b3bbb56896030b269052d63a3d899b5241ad81
kabarter <50552547+kabarter@users.noreply.github.com>
2020-12-02 15:28:01 -0500

    fixed js code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //let city = document.getElementById('displayResults')
 
 function weather(city){
-    fetch ('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=7ed6f6627d7c40973c307b393360c74e')
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q=Portland&appid=7ed6f6627d7c40973c307b393360c74e')
     .then(function(resp) {return resp.json()})
     .then(function(data){
         weatherData(data);
@@ -14,31 +14,28 @@ window.onload = function(){
     weather(city);
 }
 
+
+function weatherIcon(icon){
+    fetch('http://openweathermap.org/img/wn/10d@2x.png')
+    
+    
+}
+
+
+
 function weatherData(data){
     let fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
+    let highMax = Math.round(((parseFloat(data.main.temp_max)-273.15)*1.8)+32);
+    let lowMin = Math.round(((parseFloat(data.main.temp_min)-273.15)*1.8)+32);
+    const dateObj = new Date();
+
     document.getElementById('description').innerHTML = data.weather[0].description;
     document.getElementById('returnedLocation').innerHTML = data.name;
-    document.getElementById('returnedTemp').innerHTML = fahrenheit + ' deg';
-    document.getElementById('icon').innerHTML = description.icon;
+    document.getElementById('returnedTemp').innerHTML = fahrenheit + '&deg F currently';
+    document.getElementById('highTemp').innerHTML = highMax + '&deg F is the High';
+    document.getElementById('lowTemp').innerHTML = lowMin + '&deg F is the Low';
+    document.getElementById('date').innerHTML = dateObj;
+    document.getElementById('icon').innerHTML = data.weather[0].icon;
 
 }
 
-const search = document.querySelector('.searchCity');
-search.addEventListener('onClick', setQuery);
-
-function setQuery(event){
-    if (event.keyCode == 13){
-        getResults(search.value);
-    }
-}
-function getResults (results){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${api.7ed6f6627d7c40973c307b393360c74e}')
-    .then(results => {
-        return results.json();
-     }).then(displayResults);
-}
-
-function displayResults(weatherSearch) {
-    let city = document.querySelector('.location .city');
-    city.innerText = '${weatherSearch.name}, ${weatherSearch.sys.country}';
-}
